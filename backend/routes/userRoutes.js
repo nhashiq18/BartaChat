2637a085c53136d4e3e7934ea46fcf8dec621c30
Ashiq666 <mn.ashiq07@gmail.com>
2023-11-01 20:06:43 +0600

    search API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const registerUser = require("../controllers/registerUser");
 const loginUser = require("../controllers/loginUser");
+const allUsers = require("../controllers/searchController");
+const { protect } = require("../middleware/authMiddleware");
 
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
-router.post('/login', loginUser);
-
+router.route("/").post(registerUser).get(protect,allUsers);
+router.post("/login", loginUser);
 
 module.exports = router;
