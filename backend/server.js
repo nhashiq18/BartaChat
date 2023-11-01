c51bcb58794f0c7494fbb79054e2c59aadfa2982
Ashiq666 <mn.ashiq07@gmail.com>
2023-11-01 16:54:27 +0600

    routes error handler

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const chats = require("./data/data");
 const connectDB = require("./config/db")
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res)=>{
     res.send("API is running");
