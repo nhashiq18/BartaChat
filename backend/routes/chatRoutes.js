7a7c54e5e735b365877518e242f1d6ba6368649e
Ashiq666 <mn.ashiq07@gmail.com>
2023-11-02 23:24:40 +0600

    create group API

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
 const accessChat = require("../controllers/chatController/accessController");
 const fetchChats = require("../controllers/chatController/fetchChats");
+const createGroupChat = require("../controllers/chatController/createGroupChat");
 
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-//router.route("/group").post(protect, createGroupChat);
+router.route("/group").post(protect, createGroupChat);
 //router.route("/rename").put(protect, renameGroup);
 //router.route("/groupremove").put(protect, removeFromGroup);
 //router.route("/groupadd").put(protect, addToGroup);
